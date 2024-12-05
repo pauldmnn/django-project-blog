4e8b9e7abe64240a06cf63cb1523557b0f70c3c9
Paul-Vasile Dominan <pauldmnn@gmail.com>
2024-12-05 14:42:19 +0000

    add filter to home page

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ '8000-pauldmnn-djangoprojectb-d2yryfbp585.ws-eu117.gitpod.io',
                 '.herokuapp.com',]
