f3a0fec22b5e7d0cb3696bdfdf5127b5ead128d2
Paul-Vasile Dominan <pauldmnn@gmail.com>
2024-12-05 10:06:36 +0000

    add comment function to models

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '8000-pauldmnn-djangoprojectb-d2yryfbp585.ws-eu117.gitpod.io',
                 '.herokuapp.com',]
@@ -94,7 +94,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.codeinstitute-ide.net/",
+    "https://8000-pauldmnn-djangoprojectb-d2yryfbp585.ws-eu117.gitpod.io",
     "https://*.herokuapp.com"
 ]
 
